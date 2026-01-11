d867a23675f6f5d77a242362e9f59efa9f92aae2
Peter Wong <peter@thepwgroup.com>
2026-01-11 18:35:35 +0000

    fix: inject AppState at app root

--- ios/PetGo/PetGo/PetGoApp.swift
+++ ios/PetGo/PetGo/PetGoApp.swift
@@ -2,17 +2,13 @@ import SwiftUI
 
 @main
 struct PetGoApp: App {
+
     @StateObject private var appState = AppState()
 
     var body: some Scene {
         WindowGroup {
-            if appState.hasCompletedOnboarding {
-                RootView()
-                    .environmentObject(appState)
-            } else {
-                OnboardingView()
-                    .environmentObject(appState)
-            }
+            AppEntryView()
+                .environmentObject(appState)
         }
     }
 }
